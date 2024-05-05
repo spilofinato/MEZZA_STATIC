a047374e5b367e47a03fea693dd6588269401631
Cesare Gramatica <ce.gramatica@gmail.com>
2024-05-05 20:15:03 +0200

    fix iphone

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,13 +27,13 @@ shuffle(aPhrases);
 
 headerh1.innerHTML = aPhrases[0];
 
-const headerh1Width = headerh1.scrollWidth;
+const headerh1Width = headerh1.scrollWidth * 1.03;
 
 shuffle(aPhrases);
 
 footerh1.innerHTML = aPhrases[0];
 
-const footerh1Width = footerh1.scrollWidth;
+const footerh1Width = footerh1.scrollWidth * 1.03;
 
 const root = document.documentElement;
 
